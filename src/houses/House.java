e93d0340dc53b84d545f80fd43d982be66bc80d4
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-18 11:58:31 +0100

    House - add field, constructor

--- src/houses/House.java
+++ src/houses/House.java
@@ -3,4 +3,10 @@ package houses;
 // Создайте абстрактный класс "Дом". В качестве аргумента конструктора принимайте адрес.
 // Напишите для адреса сеттер и геттер.
 public abstract class House {
+
+  private String address;
+
+  public House(String address) {
+    this.address = address;
+  }
 }
