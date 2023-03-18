3b6c3702bf5a8246acf683d9c6cd7d800a27dbba
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-18 12:39:10 +0100

    PrivatHouse - override raze()

--- src/houses/PrivatHouse.java
+++ src/houses/PrivatHouse.java
@@ -2,6 +2,9 @@ package houses;
 
 // Создайте классы "Частный дом" и "Многоквартирный дом" - наследники класса "Дом".
 // В методе toString() выводите в удобном виде всю известную информацию о доме.
+
+// Перезапишите этот метод в "Частном доме" и "Многоквартирном доме":
+//   для частного дома выводите на экран: "Вы снесли частный дом";
 public class PrivatHouse extends House {
 
   public PrivatHouse(String address) {
@@ -11,6 +14,11 @@ public class PrivatHouse extends House {
   @Override
   public String toString() {
     return String.format("Частный дом - %s", getAddress());
+  }
+
+  @Override
+  public void raze(){
+    System.out.println("Вы снесли частный дом");
   }
 
 }
