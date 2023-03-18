2faecbade963fc0c4f44fe2688fee9c698994b2d
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-18 12:42:17 +0100

    ApartmentHouse - override raze()

--- src/houses/ApartmentHouse.java
+++ src/houses/ApartmentHouse.java
@@ -4,6 +4,10 @@ package houses;
 // При создании многоквартирного дома дополнительно принимайте в конструкторе целое
 // число - количество подъездов.
 // В методе toString() выводите в удобном виде всю известную информацию о доме.
+
+// Перезапишите этот метод в "Частном доме" и "Многоквартирном доме":
+//   для многоквартирного дома выведите на экран: "Вы снесли многоквартирный дом. %d подъездов!"
+//   Вместо %d должно быть число подъездов.
 public class ApartmentHouse extends House {
 
   private int entrance;
@@ -16,5 +20,10 @@ public class ApartmentHouse extends House {
   @Override
   public String toString() {
     return String.format("Многоквартирный дом (%d подъездов) - %s", entrance, getAddress());
+  }
+
+  @Override
+  public void raze() {
+    System.out.printf("Вы снесли многоквартирный дом. %d подъездов!", entrance);
   }
 }
