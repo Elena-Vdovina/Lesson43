cdf7ee380de2a51f5c2fce93d7c586c3550f0f8b
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-03-18 12:10:32 +0100

    PrivatHouse - add constuctor, toString()

--- src/houses/PrivatHouse.java
+++ src/houses/PrivatHouse.java
@@ -3,4 +3,14 @@ package houses;
 // Создайте классы "Частный дом" и "Многоквартирный дом" - наследники класса "Дом".
 // В методе toString() выводите в удобном виде всю известную информацию о доме.
 public class PrivatHouse extends House {
+
+  public PrivatHouse(String address) {
+    super(address);
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Частный дом - %s", getAddress());
+  }
+
 }
